9b18d80bcfc3947025f833398eb7978324887301
Håvard Fossli <hfossli@gmail.com>
2017-11-10 09:58:27 +0100

    Make code more readable and understandable

--- Sources/EllipticCurveKeyPair.swift
+++ Sources/EllipticCurveKeyPair.swift
@@ -429,8 +429,7 @@ public enum EllipticCurveKeyPair {
         }
     }
     
-    public struct Constants {
-        public static var x9_62Header: Data = Data(bytes: [UInt8]([0x30, 0x59, 0x30, 0x13, 0x06, 0x07, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x02, 0x01, 0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07, 0x03, 0x42, 0x00]))
+    public struct Constants {        
         public static let noCompression: UInt8 = 4
         public static let attrKeyTypeEllipticCurve: String = {
             if #available(iOS 10.0, *) {
@@ -443,38 +442,30 @@ public enum EllipticCurveKeyPair {
     
     public final class PublicKeyData {
         
-        /*
-        
-        OID = 06 09 2A 86 48 CE 3D 03 01 01 07
-        Comment = ANSI X9.62 named elliptic curve
-        Description = prime256v1 (1 2 840 10045 3 1 1 7)
-        Link = http://oid-info.com/get/1.2.840.10045.3.1.1.7
-        
-        OID = 06 08 2A 86 48 CE 3D 03 01 07 <---- currently in use
-        Comment = ANSI X9.62 named elliptic curve
-        Description = ansiX9p256r1 (1 2 840 10045 3 1 7)
-        Link = http://oid-info.com/get/1.2.840.10045.3.1.7
-        
-        OID = 06 08 2A 86 48 CE 3D 04 03 02
-        Comment = ANSI X9.62 ECDSA algorithm with SHA256
-        Description = ecdsaWithSHA256 (1 2 840 10045 4 3 2)
-        Link = http://oid-info.com/get/1.2.840.10045.4.3.2
-        
-         */
-        
         // As received from Security framework
         public let raw: Data
         
         // The open ssl compatible DER format X.509
         //
-        // We take the raw key and prepend an ASN.1 prefix to it. The end result is an
+        // We take the raw key and prepend an ASN.1 headers to it. The end result is an
         // ASN.1 SubjectPublicKeyInfo structure, which is what OpenSSL is looking for.
         //
         // See the following DevForums post for more details on this.
+        // https://forums.developer.apple.com/message/84684#84684
         //
-        // <https://forums.developer.apple.com/message/84684#84684>.
+        // End result looks like this
+        // https://lapo.it/asn1js/#3059301306072A8648CE3D020106082A8648CE3D030107034200041F4E3F6CD8163BCC14505EBEEC9C30971098A7FA9BFD52237A3BCBBC48009162AAAFCFC871AC4579C0A180D5F207316F74088BF01A31F83E9EBDC029A533525B
+        //
         public lazy var DER: Data = {
-            var result = Constants.x9_62Header
+            var x9_62HeaderECHeader = [UInt8]([
+                /* sequence          */ 0x30, 0x59,
+                /* |-> sequence      */ 0x30, 0x13,
+                /* |---> ecPublicKey */ 0x06, 0x07, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x02, 0x01, // http://oid-info.com/get/1.2.840.10045.2.1 (ANSI X9.62 public key type)
+                /* |---> prime256v1  */ 0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, // http://oid-info.com/get/1.2.840.10045.3.1.7 (ANSI X9.62 named elliptic curve)
+                /* |-> bit headers   */ 0x07, 0x03, 0x42, 0x00
+                ])
+            var result = Data()
+            result.append(Data(x9_62HeaderECHeader))
             result.append(self.raw)
             return result
         }()
